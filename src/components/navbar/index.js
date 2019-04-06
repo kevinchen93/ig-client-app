eee8e20fc24a9d8ab430446b7ca6e34713e0d87d
Kevin Chen <xkevinchen@gmail.com>
2019-04-05 22:58:50 -0400

    Initial commit

---
+++ src/components/navbar/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './index.scss';
+
+export default class Navbar extends Component {
+  render() {
+    <nav className="navbar-container">This is the navbar.</nav>
+  }
+}
